cdefd08b0d17068765246bba8de3fe152ec3f850
Creativoo <creativoo@gmail.com>
2023-09-11 13:49:14 -0700

    terminando portafolio accesible

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ window.onload = () => {
   document.querySelector(".arrow-left").addEventListener("click", clickLeft);
   document
     .querySelector(".send-button")
-    .addEventListener("click", showNotification);
+    .addEventListener("click", (e) => validateForm(e));
   document.querySelectorAll(".project").forEach((element) => {
     element.addEventListener("click", (e) => openModal(e));
   });
   document.body.addEventListener("click", (e) => closeModal(e));
+  document.body.addEventListener("keyup", (e) => listenForEsc(e));
 };
 
 /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
@@ -25,11 +26,24 @@ function clickRight() {
   switch (newValue) {
     case -270:
       document.querySelector(".project1").setAttribute("tabindex", "-1");
-      document.querySelector("project4").removeAttribute("tabindex");
+      document
+        .querySelector(".project1-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project4").removeAttribute("tabindex");
+      document
+        .querySelector(".project4-container")
+        .removeAttribute("aria-hidden");
       break;
     case -540:
       document.querySelector(".project2").setAttribute("tabindex", "-1");
-      document.querySelector("project5").removeAttribute("tabindex");
+      document
+        .querySelector(".project2-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project5").removeAttribute("tabindex");
+      document
+        .querySelector(".project5-container")
+        .removeAttribute("aria-hidden");
+
       break;
     default:
       break;
@@ -50,29 +64,67 @@ function clickLeft() {
   document.querySelector(".project-container").style.left = `${newValue}px`;
 }
 
+/** validar el formulario antes de enviarlo */
+function validateForm(e) {
+  e.preventDefault();
+  const nameField = document.getElementById("name");
+  if (nameField.value === "") {
+    document.getElementById("name-error").innerHTML =
+      "! Para enviar el formulario, se necesita un nombre";
+  } else {
+    showNotification();
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
 function showNotification() {
+  document.getElementById("name-error").innerHTML = "";
+  document.getElementById(".form-container").reset();
+  document.querySelector(".notification").innerHTML =
+    "El formulario fue enviado exitosamente";
   document.querySelector(".notification").style.display = "flex";
+
   setTimeout(function () {
     document.querySelector(".notification").style.display = "none";
   }, 3000);
   switch (newValue) {
     case -270:
       document.querySelector(".project5").setAttribute("tabindex", "-1");
+
+      document
+        .querySelector(".project5-container")
+        .setAttribute("aria-hidden", "true");
       document.querySelector("project2").removeAttribute("tabindex");
+      document
+        .querySelector(".project2-container")
+        .removeAttribute("aria-hidden");
       break;
     case 0:
       document.querySelector(".project4").setAttribute("tabindex", "-1");
-      document.querySelector("project1").removeAttribute("tabindex");
+      document
+        .querySelector(".project4-container")
+        .setAttribute("aria-hidden", "true");
+      document.querySelector(".project1").removeAttribute("tabindex");
+      document
+        .querySelector(".project1-container")
+        .removeAttribute("aria-hidden");
       break;
     default:
       break;
   }
 }
 
+/** escucha por la clave ESC para cerrar el modal */
+function listenForEsc(e) {
+  if (e.keyCode === 27) {
+    closeModal(e);
+  }
+}
+
 /** Esta funcion se llama cuando la persona hace click en cualquier porjecto del carousel */
 function openModal(e) {
   document.querySelector(".modal-container").style.display = "flex";
+  document.getElementById("modal-header").focus();
 }
 
 /** Esta funcion se llama para cerrar el modal */
